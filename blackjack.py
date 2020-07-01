24870d2c7ef79cecfa670afbae79eef668807140
CuriousExpert24 <karthikbaga04@gmail.com>
2020-06-30 21:54:49 -0500

    Added methods in a value class

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import random
+
+
+class values:
+    @classmethod
+    def findAceValue(cls, totalValue):
+        if totalValue + 11 > 21:
+            return 1
+        else:
+            return 11
+
+    @classmethod
+    def canContinue(cls, totalValue):
+        if totalValue > 21:
+            return False
+        else:
+            return True
